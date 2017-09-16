462247b60f8e85635e8161a88d6261c46f9134c4
Thiago Castilho <tvitor.castilho@gmail.com>
2017-09-16 14:51:23 -0400

    .gitignore is now working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { MainApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -13,7 +13,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
-    MyApp,
+    MainApp,
     AboutPage,
     ContactPage,
     HomePage,
@@ -21,11 +21,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MainApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    MainApp,
     AboutPage,
     ContactPage,
     HomePage,
